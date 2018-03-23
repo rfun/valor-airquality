505ceb03a47eec7f34add1769318553d692bffa6
Rohit Khattar <rohitkh@byu,edu>
2018-03-22 19:07:00 -0600

    Inital Commit

---
+++ tethysapp/valor_airquality/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class ValorAirquality(TethysAppBase):
+    """
+    Tethys app class for Team Valor   Air Quality.
+    """
+
+    name = 'Team Valor   Air Quality'
+    index = 'valor_airquality:home'
+    icon = 'valor_airquality/images/icon.gif'
+    package = 'valor_airquality'
+    root_url = 'valor-airquality'
+    color = '#2c3e50'
+    description = 'Place a brief description of your app here.'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='valor-airquality',
+                controller='valor_airquality.controllers.home'
+            ),
+        )
+
+        return url_maps
