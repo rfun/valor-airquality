29cf862bba45ad1b187cdf81c1caa309632ca9b8
Josh Gibbons <jdgibbons19@gmail.com>
2018-03-27 08:52:59 -0600

    added logo and description via app.py

--- tethysapp/valor_airquality/app.py
+++ tethysapp/valor_airquality/app.py
@@ -8,11 +8,11 @@ class ValorAirquality(TethysAppBase):
 
     name = 'Team Valor   Air Quality'
     index = 'valor_airquality:home'
-    icon = 'valor_airquality/images/icon.gif'
+    icon = 'valor_airquality/images/AQLogo.png'
     package = 'valor_airquality'
     root_url = 'valor-airquality'
     color = '#2c3e50'
-    description = 'Place a brief description of your app here.'
+    description = 'This app provides air quality index estimates in Salt Lake County based on station data and date. Select a point or route to determine the air quality in the surrounding area on a particular day, measured by the Air Quality Index (AQI).'
     tags = ''
     enable_feedback = False
     feedback_emails = []
